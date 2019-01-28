88f88b9254f8153cd9e7e090b771f77ef63b1c3c
jught <37115117+jught@users.noreply.github.com>
2019-01-28 15:52:21 +0100

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+//Programe un algoritmo que, dados dos números enteros que entran como datos, indique si uno es divisor del otro.
+//http://www.unca.edu.mx/Cprog/archivos/GuiaEjercicios_C_Cprog_UNCA_2012.pdf
+char* traduce(int n){
+    if (n == 0) {return "no";}
+    else {return "sí";}
+    }
+int main()
+{  
+    float n = 150;
+    float m = 300;
+    float r = 150%300;
+    printf("%f  %s es divisor de %f", m, traduce(r == 0) ,n);
+    return 0;
+}
